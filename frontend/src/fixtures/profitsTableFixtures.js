943918451f2d423f9c76ae59f313fd0df70733e3
Tyler Ekaireb <tylerekaireb@gmail.com>
2022-03-04 09:43:34 -0800

    te - added frontend tests for Profits and ProfitsTable, reached 100% jest coverage

---
+++ frontend/src/fixtures/profitsTableFixtures.js
@@ -0,0 +1,24 @@
+const profitsTableFixtures = {
+    threeFormattedProfits: [
+        {
+            "id": 1,
+            "profit": 100,
+            "timestamp": 1641024000000,
+            "date": "1/1/2022"
+        },
+        {
+            "id": 2,
+            "profit": 101,
+            "timestamp": 1641110400000,
+            "date": "1/2/2022"
+        },
+        {
+            "id": 3,
+            "profit": 102,
+            "timestamp": 1642665600000,
+            "date": "1/20/2022"
+        }
+    ],
+}
+
+export default profitsTableFixtures;
--- frontend/src/main/components/Commons/Profits.js
+++ frontend/src/main/components/Commons/Profits.js
@@ -2,14 +2,12 @@ import React from "react";
 import { Card } from "react-bootstrap";
 import ProfitsTable from "main/components/Commons/ProfitsTable"
 
-import userCommonsFixtures from "fixtures/userCommonsFixtures";
-
-const dummyData = [
-    { id: 1, profit: 10, date: "2021-03-05" },
-    { id: 2, profit: 11, date: "2021-03-06" },
-    { id: 3, profit: 10, date: "2021-03-07" },
-    { id: 4, profit: 8, date: "2021-03-08" }
-];
+// const dummyData = [
+//     { id: 1, profit: 10, date: "2021-03-05" },
+//     { id: 2, profit: 11, date: "2021-03-06" },
+//     { id: 3, profit: 10, date: "2021-03-07" },
+//     { id: 4, profit: 8, date: "2021-03-08" }
+// ];
 
 // Helper function that accepts a timestamp and returns date as string
 function format_date(s) {
