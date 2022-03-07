fc681f072c004e47180f4c93223dd5558bc46105
zjw963546 <xsdzhengjianwei@163.com>
2022-03-07 15:35:08 -0800

    make more tests for AdminDisplayTablePage.js

--- frontend/src/tests/pages/AdminDisplayTablePage.test.js
+++ frontend/src/tests/pages/AdminDisplayTablePage.test.js
@@ -4,6 +4,7 @@ import AdminDisplayTablePage from "main/pages/AdminDisplayTablePage";
 import { MemoryRouter } from "react-router-dom";
 import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+import commonsFixtures from "fixtures/commonsFixtures";
 
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
@@ -19,11 +20,22 @@ jest.mock('react-router-dom', () => {
     };
 });
 
+const mockToast = jest.fn();
+jest.mock('react-toastify', () => {
+    const originalModule = jest.requireActual('react-toastify');
+    return {
+        __esModule: true,
+        ...originalModule,
+        toast: (x) => mockToast(x)
+    };
+});
+
 
 describe("AdminDisplayTablePage tests", () => {
 
     const axiosMock = new AxiosMockAdapter(axios);
     const queryClient = new QueryClient();
+    const testId = "DisplayTable";
 
 
     beforeEach(() => {
@@ -44,6 +56,79 @@ describe("AdminDisplayTablePage tests", () => {
         );
 
         await waitFor(() => expect(getByText("Admin Display Table Page")).toBeInTheDocument());
+
+    });
+
+
+    test("renders three commons without crashing for admin user", async () => {
+        const queryClient = new QueryClient();
+        axiosMock.onGet("/api/commons/all").reply(200, commonsFixtures.threeCommons);
+
+        const { getByTestId } = render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <AdminDisplayTablePage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        await waitFor(() => { expect(getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("5"); });
+        expect(getByTestId(`${testId}-cell-row-1-col-id`)).toHaveTextContent("4");
+        expect(getByTestId(`${testId}-cell-row-2-col-id`)).toHaveTextContent("1");
+
+    });
+
+
+    // test("renders empty table when backend unavailable", async () => {
+
+    //     const queryClient = new QueryClient();
+    //     axiosMock.onGet("/api/commons/all").timeout();
+
+    //     const restoreConsole = mockConsole();
+
+    //     const { queryByTestId } = render(
+    //         <QueryClientProvider client={queryClient}>
+    //             <MemoryRouter>
+    //                 <AdminDisplayTablePage />
+    //             </MemoryRouter>
+    //         </QueryClientProvider>
+    //     );
+
+    //     await waitFor(() => { expect(axiosMock.history.get.length).toBeGreaterThanOrEqual(1); });
+
+    //     const errorMessage = console.error.mock.calls[0][0];
+    //     expect(errorMessage).toMatch("Error communicating with backend via GET on /api/commons/all");
+    //     restoreConsole();
+
+    //     expect(queryByTestId(`${testId}-cell-row-0-col-id`)).not.toBeInTheDocument();
+    // });
+
+    test("test what happens when you click delete", async () => {
+
+        const queryClient = new QueryClient();
+        axiosMock.onGet("/api/commons/all").reply(200, commonsFixtures.threeCommons);
+        axiosMock.onDelete("/api/commons/delete").reply(200, "record 5 was deleted");
+
+
+        const { getByTestId } = render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <AdminDisplayTablePage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        await waitFor(() => { expect(getByTestId(`${testId}-cell-row-0-col-id`)).toBeInTheDocument(); });
+
+       expect(getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("5"); 
+
+
+        const deleteButton = getByTestId(`${testId}-cell-row-0-col-Delete-button`);
+        expect(deleteButton).toBeInTheDocument();
+       
+        fireEvent.click(deleteButton);
+
+        await waitFor(() => { expect(mockToast).toBeCalledWith("record 5 was deleted") });
 
     });
 
