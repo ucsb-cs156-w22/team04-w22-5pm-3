0d96688b9e178aa0c6edba3d84c05e30eaaca4ac
Kenneth Deng <kdeng@umail.ucsb.edu>
2022-03-07 13:06:18 -0800

    display table component works with dummny data

---
+++ frontend/src/tests/components/Commons/DisplayTable.test.js
@@ -0,0 +1,64 @@
+import { render } from "@testing-library/react";
+import DisplayTable from "main/components/Commons/DisplayTable";
+import commonsFixtures from "fixtures/commonsFixtures";
+import { currentUserFixtures } from "fixtures/currentUserFixtures";
+
+import { QueryClient, QueryClientProvider } from "react-query";
+import { MemoryRouter } from "react-router-dom";
+
+describe("AdminDisplayTable tests", () => {
+    const queryClient = new QueryClient();
+
+    test("renders without crashing for empty table for admin", () => {
+        const currentUser = currentUserFixtures.adminUser;
+
+        render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <DisplayTable commons={[]} currentUser={currentUser} />
+                </MemoryRouter>
+            </QueryClientProvider>
+
+        );
+    });
+
+    test("Has the expected colum headers and content for adminUser", () => {
+
+        const currentUser = currentUserFixtures.adminUser;
+
+        const { getByText, getByTestId } = render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <DisplayTable commons={commonsFixtures.threeCommons} currentUser={currentUser} />
+                </MemoryRouter>
+            </QueryClientProvider>
+
+        );
+
+        const expectedHeaders = ["ID#", "Name", "Starting Balance", "Cow Price", "Milk Price", "Start Date"];
+        const expectedFields = ["id", "startingBalance", "cowPrice", "milkPrice", "startDate"];
+        const testId = "DisplayTable";
+
+        expectedHeaders.forEach((headerText) => {
+            const header = getByText(headerText);
+            expect(header).toBeInTheDocument();
+        });
+
+        expectedFields.forEach((field) => {
+            const header = getByTestId(`${testId}-cell-row-0-col-${field}`);
+            expect(header).toBeInTheDocument();
+        });
+
+        expect(getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("5");
+        expect(getByTestId(`${testId}-cell-row-1-col-id`)).toHaveTextContent("4");
+
+        // const editButton = getByTestId(`${testId}-cell-row-0-col-Edit-button`);
+        // expect(editButton).toBeInTheDocument();
+        // expect(editButton).toHaveClass("btn-primary");
+
+        const deleteButton = getByTestId(`${testId}-cell-row-0-col-Delete-button`);
+        expect(deleteButton).toBeInTheDocument();
+        expect(deleteButton).toHaveClass("btn-danger");
+
+    });
+});
