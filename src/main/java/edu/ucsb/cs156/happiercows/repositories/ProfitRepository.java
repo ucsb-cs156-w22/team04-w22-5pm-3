da6c7562549d0334fa7ebabc903e8094322e7b98
Tyler Ekaireb <tylerekaireb@gmail.com>
2022-03-07 14:54:18 -0800

    te - removed  function definition from , per @btk5h's recommendation

--- src/main/java/edu/ucsb/cs156/happiercows/repositories/ProfitRepository.java
+++ src/main/java/edu/ucsb/cs156/happiercows/repositories/ProfitRepository.java
@@ -7,6 +7,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ProfitRepository extends CrudRepository<Profit, Long> {
   Iterable<Profit> findAllByUserCommonsId(Long user_commons_id);
-
-  Iterable<Profit> findAll();
 }
