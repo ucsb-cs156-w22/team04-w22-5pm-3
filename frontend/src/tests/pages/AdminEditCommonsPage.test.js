30bf9eab7db37df10e106d809fede879eadd4683
Jason Em <jasonem@umail.ucsb.edu>
2022-03-10 13:05:15 -0800

    got AdminEditCommonsPage tests working

--- frontend/src/tests/pages/AdminEditCommonsPage.test.js
+++ frontend/src/tests/pages/AdminEditCommonsPage.test.js
@@ -18,47 +18,50 @@ jest.mock('react-toastify', () => {
     };
 });
 
-const mockedNavigate = jest.fn();
+const mockNavigate = jest.fn();
 jest.mock('react-router-dom', () => {
     const originalModule = jest.requireActual('react-router-dom');
     return {
         __esModule: true,
         ...originalModule,
-        Navigate: (x) => { mockedNavigate(x); return null; }
+        useParams: () => ({
+            id: 1
+        }),
+        Navigate: (x) => { mockNavigate(x); return null; }
     };
 });
 
+describe("AdminEditCommonsPagePage tests", () => {
 
-describe("when the backend doesn't return a todo", () => {
+    describe("when the backend doesn't return a todo", () => {
 
-    const axiosMock = new AxiosMockAdapter(axios);
-    const queryClient = new QueryClient();
+        const axiosMock = new AxiosMockAdapter(axios);
 
-    beforeEach(() => {
-        axiosMock.reset();
-        axiosMock.resetHistory();
-        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
-        axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
-        axiosMock.onGet("/api/commons", { params: { id: 1 } }).timeout();
-    });
+        beforeEach(() => {
+            axiosMock.reset();
+            axiosMock.resetHistory();
+            axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
+            axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
+            axiosMock.onGet("/api/commons", { params: { id: 1 } }).timeout();
+        });
 
-    
-    test("renders header but table is not present", async () => {
-        const {getByText, queryByTestId} = render(
-            <QueryClientProvider client={queryClient}>
-                <MemoryRouter>
-                    <AdminEditCommonsPage />
-                </MemoryRouter>
-            </QueryClientProvider>
-        );
-        await waitFor(() => expect(getByText("Edit Common")).toBeInTheDocument());
+        const queryClient = new QueryClient();
+        test("renders header but table is not present", async () => {
+            const {getByText, queryByTestId} = render(
+                <QueryClientProvider client={queryClient}>
+                    <MemoryRouter>
+                        <AdminEditCommonsPage />
+                    </MemoryRouter>
+                </QueryClientProvider>
+            );
+            await waitFor(() => expect(getByText("Edit Common")).toBeInTheDocument());
+            expect(queryByTestId("EditCommonsForm-name")).not.toBeInTheDocument();
+        });
     });
 
     describe("tests where backend is working normally", () => {
 
         const axiosMock = new AxiosMockAdapter(axios);
-        const queryClient = new QueryClient();
-    
     
         beforeEach(() => {
             axiosMock.reset();
@@ -66,23 +69,23 @@ describe("when the backend doesn't return a todo", () => {
             axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
             axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
             axiosMock.onGet("/api/commons", { params: { id: 1 } }).reply(200, {
-                id: 1,
+                id: "1",
                 name: '1',
-                startingDate: "6/10/2021",
+                startingDate: "2018-12-01T08:30:00",
                 cowPrice: "1",
                 milkPrice: "1",
                 startingBalance: "1"
             });
             axiosMock.onPut('/api/commons').reply(200, {
                 id: "1",
-                name: '1',
-                cowPrice: "1",
-                milkPrice: "1",
-                startingBalance: "1"
+                name: '2',
+                cowPrice: "2",
+                milkPrice: "2",
+                startingBalance: "2"
             });
         });
 
-        
+        const queryClient = new QueryClient();
         test("renders without crashing", () => {
             render(
                 <QueryClientProvider client={queryClient}>
@@ -96,33 +99,32 @@ describe("when the backend doesn't return a todo", () => {
         test("Is populated with the data provided", async () => {
 
             
-            const { getByText, getByLabelText } = render(
+            const { getByTestId } = render(
                 <QueryClientProvider client={queryClient}>
                     <MemoryRouter>
                         <AdminEditCommonsPage />
                     </MemoryRouter>
                 </QueryClientProvider>
             );
-            await waitFor(() => expect(getByText("Edit Common")).toBeInTheDocument());
 
+            await waitFor(() => expect(getByTestId("EditCommonsForm-startingBalance")).toBeInTheDocument());
             
-            const commonsNameField = getByLabelText("Commons Name");
-            const startingBalanceField = getByLabelText("Starting Balance");
-            const cowPriceField = getByLabelText("Cow Price");
-            const milkPriceField = getByLabelText("Milk Price");
+            const commonsNameField = getByTestId("EditCommonsForm-name");
+            const startingBalanceField = getByTestId("EditCommonsForm-startingBalance");
+            const cowPriceField = getByTestId("EditCommonsForm-cowPrice");
+            const milkPriceField = getByTestId("EditCommonsForm-milkPrice");
             
-
             expect(commonsNameField).toHaveValue("1");
-            expect(startingBalanceField).toHaveValue("1");
-            expect(cowPriceField).toHaveValue("1");
-            expect(milkPriceField).toHaveValue("1");
+            expect(startingBalanceField).toHaveValue(1);
+            expect(cowPriceField).toHaveValue(1);
+            expect(milkPriceField).toHaveValue(1);
         });
 
         test("Changes when you click Update", async () => {
 
 
 
-            const { getByText, getByLabelText, getByTestId } = render(
+            const { getByText, getByTestId } = render(
                 <QueryClientProvider client={queryClient}>
                     <MemoryRouter>
                         <AdminEditCommonsPage />
@@ -131,37 +133,37 @@ describe("when the backend doesn't return a todo", () => {
             );
             await waitFor(() => expect(getByText("Edit Common")).toBeInTheDocument());
 
-            
-            const commonsNameField = getByLabelText("Commons Name");
-            const startingBalanceField = getByLabelText("Starting Balance");
-            const cowPriceField = getByLabelText("Cow Price");
-            const milkPriceField = getByLabelText("Milk Price");
+            const commonsNameField = getByTestId("EditCommonsForm-name");
+            const startingBalanceField = getByTestId("EditCommonsForm-startingBalance");
+            const cowPriceField = getByTestId("EditCommonsForm-cowPrice");
+            const milkPriceField = getByTestId("EditCommonsForm-milkPrice");
+            const submitButton = getByTestId("EditCommonsForm-submit");
 
             expect(commonsNameField).toHaveValue("1");
-            expect(startingBalanceField).toHaveValue("1");
-            expect(cowPriceField).toHaveValue("1");
-            expect(milkPriceField).toHaveValue("1");
+            expect(startingBalanceField).toHaveValue(1);
+            expect(cowPriceField).toHaveValue(1);
+            expect(milkPriceField).toHaveValue(1);
 
             expect(submitButton).toBeInTheDocument();
 
-            fireEvent.change(commonsNameField, { target: { value: '2' } })
-            fireEvent.change(startingBalanceField, { target: { value: '2' } })
-            fireEvent.change(cowPriceField, { target: { value: '2' } })
-            fireEvent.change(milkPriceField, { target: { value: '2' } })
+            fireEvent.change(commonsNameField, { target: { value: "2" } })
+            fireEvent.change(startingBalanceField, { target: { value: 2 } })
+            fireEvent.change(cowPriceField, { target: { value: 2 } })
+            fireEvent.change(milkPriceField, { target: { value: 2 } })
 
             fireEvent.click(submitButton);
 
             await waitFor(() => expect(mockToast).toBeCalled);
-            expect(mockToast).toBeCalledWith("Commons Updated - id: 2 name: 2");
+            expect(mockToast).toBeCalledWith("Common Updated - id: 1 name: 2");
             expect(mockNavigate).toBeCalledWith({ "to": "/admin/displaytable" });
 
             expect(axiosMock.history.put.length).toBe(1); // times called
-            expect(axiosMock.history.put[0].params).toEqual({ id: 17 });
+            expect(axiosMock.history.put[0].params).toEqual({ id: "1" });
             expect(axiosMock.history.put[0].data).toBe(JSON.stringify({
-                commonsNameField: '2',
-                startingBalanceField: "2",
-                cowPriceField: "2",
-                milkPriceField: "2"
+                name: '2',
+                cowPrice: 2,
+                milkPrice: 2,
+                startingBalance: 2
             })); // posted object
 
         });
