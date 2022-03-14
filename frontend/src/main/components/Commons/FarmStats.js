0a65b22958fea029657b950595baf96cbaf7921f
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2022-03-13 18:18:03 -0700

    mw - add optional chaining to wealth and health stats

--- frontend/src/main/components/Commons/FarmStats.js
+++ frontend/src/main/components/Commons/FarmStats.js
@@ -16,13 +16,13 @@ const FarmStats = ({ userCommons }) => {
           <img class="icon" src={Cash} alt="Cash"></img>
         </Card.Text>
         <Card.Text>
-          Total Wealth: ${userCommons?.totalWealth.toFixed(2)}
+          Total Wealth: ${userCommons?.totalWealth?.toFixed(2)}
         </Card.Text>
         <Card.Text>
           <img class="icon" src={Health} alt="Health"></img>
         </Card.Text>
         <Card.Text>
-          Cow Health: {parseFloat(userCommons?.cowHealth.toFixed(2))}%
+          Cow Health: {parseFloat(userCommons?.cowHealth?.toFixed(2))}%
         </Card.Text>
         <Card.Text>
           <progress id="health" value={userCommons?.cowHealth}
